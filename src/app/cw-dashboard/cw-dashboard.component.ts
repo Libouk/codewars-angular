6aaa70fff68fa33b6b2a53d762198813a6b65fa7
Quentin Lebouc <lebouc.quentin@gmail.com>
2018-09-28 17:32:11 +0200

    Getting hands on with material card component

--- src/app/cw-dashboard/cw-dashboard.component.ts
+++ src/app/cw-dashboard/cw-dashboard.component.ts
@@ -21,10 +21,13 @@ export class CwDashboardComponent {
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Card 1', cols: 2, rows: 1, description: 'tata' },
+        { title: 'Card 2', cols: 1, rows: 1, description: 'tete' },
+        { title: 'Card 3', cols: 1, rows: 2, description: 'tyty' },
+        { title: 'Card 4', cols: 1, rows: 1, description: 'tutu' },
+        { title: 'Card 5', cols: 1, rows: 1, description: 'titi' },
+        { title: 'Card 6', cols: 1, rows: 1, description: 'toto' },
+        { title: 'Card 7', cols: 1, rows: 1, description: 'tptp' }
       ];
     })
   );
